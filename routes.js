75b75123492ab36d7a45f7b67e9ea63d1c92c7c4
greemaltend4 <dengestabiy@hotmail.com>
2025-09-03 12:22:56 -0500

    Refactor response message for clarity

--- routes.js
+++ routes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 router.get('/sync', (req, res) => {
     // Logic to synchronize data
-    res.send({ message: 'Data has been synchronized!', status: 'success' });
+    res.send({ message: 'Data synchronization complete.', status: 'success' });
 });
 
 module.exports = router;
