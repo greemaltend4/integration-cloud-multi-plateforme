b349943658735dba9f00d5371f07e11530404516
greemaltend4 <dengestabiy@hotmail.com>
2025-09-03 12:16:04 -0500

    Update success message structure in /sync route.

--- routes.js
+++ routes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 router.get('/sync', (req, res) => {
     // Logic to synchronize data
-    res.send({ message: 'Data synchronized!', status: 'success' });
+    res.send({ message: 'Data synchronized successfully!', status: 'success' });
 });
 
 module.exports = router;
