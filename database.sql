62daf01e5bf4c9bf899640435cecb049e350ddcb
greemaltend4 <dengestabiy@hotmail.com>
2025-09-03 12:15:12 -0500

    Add indexes for optimization on utilisateurs and synchronisations tables

--- database.sql
+++ database.sql
@@ -3,7 +3,8 @@ CREATE TABLE utilisateurs (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nom VARCHAR(100) NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
-    mot_de_passe VARCHAR(255) NOT NULL
+    mot_de_passe VARCHAR(255) NOT NULL,
+    INDEX (email)  -- Added index on email for faster search
 );
 
 CREATE TABLE synchronisations (
@@ -11,5 +12,6 @@ CREATE TABLE synchronisations (
     utilisateur_id INT,
     service VARCHAR(100),
     date_synchronisation DATETIME DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (utilisateur_id) REFERENCES utilisateurs(id)
+    FOREIGN KEY (utilisateur_id) REFERENCES utilisateurs(id),
+    INDEX (utilisateur_id)  -- Added index on utilisateur_id for faster joins
 );
